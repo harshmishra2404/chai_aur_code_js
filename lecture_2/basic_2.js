e78955fd40a3b9940631a50d7616c699c56b0cb9
harshmishra2404 <harshm2404@gmail.com>
2025-04-30 20:28:06 +0000

    kuch changes hai

---
+++ lecture_2/basic_2.js
@@ -0,0 +1,7 @@
+accountEmail = "harshm2404@gmail.com";
+accountId = "1234"
+const accountPassword = 123456;
+console.log(accountId,accountPassword,accountEmail);
+
+console.table([accountId,accountPassword,accountEmail]);  // table---> ye bas bas table ke form main print karne ke liye use hota hai
+
